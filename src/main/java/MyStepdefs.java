dccdcdee4a42ea95c34d28ba9485d9fe93f9ad46
Grzegorz <grz>
2019-08-25 14:30:34 +0200

    Commit

---
+++ src/main/java/MyStepdefs.java
@@ -0,0 +1,17 @@
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+
+public class MyStepdefs {
+    @Given("^As an unregistered user$")
+    public void asAnUnregisteredUser() {
+    }
+
+    @When("^I go on webside https://www\\.phptravels\\.net/register$")
+    public void iGoOnWebsideHttpsWwwPhptravelsNetRegister() {
+    }
+
+    @And("^I put valid data - First Name, Last Name, email and set a password$")
+    public void iPutValidDataFirstNameLastNameEmailAndSetAPassword() {
+    }
+}
